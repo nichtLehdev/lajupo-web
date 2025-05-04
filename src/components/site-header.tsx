598d5e4194a32f619c0eec29b3607e5d14f25fd4
Lehdev <lars@lehdev.de>
2025-05-04 14:29:49 +0200

    upgraded to tailwind 3

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -17,8 +17,8 @@ export async function SiteHeader() {
   const user = await currentUser();
 
   return (
-    <header className="sticky top-0 z-50 w-full border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-screen-2xl items-center">
+    <header className="sticky top-0 z-50 w-full border-border/40 bg-background/95 backdrop-blur-sm supports-backdrop-filter:bg-background/60">
+      <div className="container flex h-14 max-w-(--breakpoint-2xl) items-center">
         <MainNav />
         <MobileNav />
         <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
