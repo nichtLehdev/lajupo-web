fa16014d75e95bd080d537f1d3075b6db419c538
Lars <lars@lehdev.de>
2025-09-29 11:40:57 +0200

    Update concert configuration with new details

--- src/config/event.ts
+++ src/config/event.ts
@@ -18,18 +18,18 @@ type Concert = {
 
 export const concertConfig: Concert = {
   title: "TBA",
-  date: new Date("2025-09-28").toLocaleDateString("de-DE", {
+  date: new Date("2026-03-22").toLocaleDateString("de-DE", {
     year: "numeric",
     month: "2-digit",
     day: "2-digit",
   }),
-  time: "18:00",
+  time: "TBA",
   location: {
-    name: "Willibrordi-Dom",
-    address: "Großer Markt",
-    city: "Wesel",
-    zip: "46483",
-    link: "https://www.google.com/maps/dir//Willibrordi-Dom",
+    name: "TBA,
+    address: "",
+    city: "Simmern",
+    zip: "55469",
+    link: "",
   },
-  link: "https://landing.churchdesk.com/e/35781340/WESELER-DOMKONZERT---Bl%C3%A4ser-und-Orgel",
+  link: "",
 };
